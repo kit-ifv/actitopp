8e4ea4a06e5a557db7c92fea7baf6080d1dcc02c
Robin Andre <fi7528@kit.edu>
2025-04-16 10:29:46 +0200

    No need for such a weird method

--- src/main/java/edu/kit/ifv/mobitopp/actitopp/enums/ActivityType.kt
+++ src/main/java/edu/kit/ifv/mobitopp/actitopp/enums/ActivityType.kt
@@ -2,7 +2,7 @@ package edu.kit.ifv.mobitopp.actitopp.enums
 
 import java.util.Collections
 import java.util.EnumSet
-
+// TODO find out whether Unknown is deliberately set to a lower case letter so that the getTypeFromChar Throws
 enum class ActivityType(val typeasChar: Char) {
     WORK('W'),
     EDUCATION('E'),
@@ -36,14 +36,8 @@ enum class ActivityType(val typeasChar: Char) {
         )
 
         fun getTypeFromChar(charValue: Char): ActivityType {
-            val tocompare = charValue.uppercaseChar()
+            return entries.firstOrNull {it.typeasChar == charValue.uppercaseChar()} ?: throw NoSuchElementException("There is no type $charValue as activity in actiTopp")
 
-            for (type in EnumSet.allOf(ActivityType::class.java)) {
-                if (type.typeasChar == tocompare) {
-                    return type
-                }
-            }
-            throw NoSuchElementException("There is no type $charValue as activity in actiTopp")
         }
     }
 }
