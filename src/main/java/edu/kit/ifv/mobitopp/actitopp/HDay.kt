b25b0398e1e399314797682b0cf710e55d61dc52
fi7528 <fi7528@kit.edu>
2025-04-24 10:02:41 +0200

    changing field access get() to simple fields, if the underlying property is immutable

--- src/main/java/edu/kit/ifv/mobitopp/actitopp/HDay.kt
+++ src/main/java/edu/kit/ifv/mobitopp/actitopp/HDay.kt
@@ -17,102 +17,15 @@ class HDay(parent: HWeekPattern, val weekday: DayOfWeek) {
 
 
 
+    // Does not need to be get() method if person never changes
+    val person: ActitoppPerson = pattern.person
 
-    val person: ActitoppPerson
-        get() = pattern.person
+    /**
+     * Does not need to be get(). Returns the index of the Day similar to DayOfWeek.oridnal()
+     * Mo = 0, So = 6
+     */
+    val index: Int = weekday.value - 1
 
-    fun dayss(){
-        DayOfWeek.MONDAY.value
-    }
-
-    val index: Int
-        /**
-         * returns the weekday index
-         *
-         *
-         * 0 - Monday
-         * 1 - Tuesday
-         * 2 - Wednesday
-         * 3 - Thursday
-         * 4 - Friday
-         * 5 - Saturday
-         * 6 - Sunday
-         *
-         * @return
-         */
-        get() = weekday.value - 1
-
-
-    fun hasActivity(activityType: ActivityType): Boolean {
-        return tours.any { t -> t.activities.any { it.activityType == activityType && it.isScheduled } }
-    }
-    fun addTour(tour: HTour) {
-        assert(tour.index != -99) { "index of the tour is not initialized" }
-        var tourindexexisitiert = false
-        for (tmptour in tours) {
-            if (tmptour.index == tour.index) tourindexexisitiert = true
-        }
-        assert(!tourindexexisitiert) { "a tour using this index already exists" }
-        tours.add(tour)
-    }
-
-
-    override fun toString(): String {
-        return "Wochentag " + weekday +
-                " #Touren " + amountOfTours +
-                " Haupttour: " + mainTourType
-    }
-
-    fun printDayPattern(): String {
-        var result = "Day " + index + " // "
-
-        for (tmpact in allActivitiesoftheDay) {
-            result = result + " " + tmpact.index + " " + tmpact.activityType.typeasChar
-            if (tmpact.isActivityLastinTour) result = "$result //"
-        }
-        return result
-    }
-
-    val isHomeDay: Boolean
-        get() = (amountOfTours == 0)
-
-    val allActivitiesoftheDay: List<HActivity>
-        get() {
-            val allactivities: MutableList<HActivity> = ArrayList()
-            for (tmptour in this.tours) {
-                allactivities.addAll(tmptour.activities)
-            }
-            return allactivities
-        }
-
-    val mainTourType: ActivityType?
-        get() = getTour(0).getActivity(0).activityType
-
-
-    val totalAmountOfActivitites: Int
-        /**
-         * returns the amount of activities without home activities
-         *
-         * @return
-         */
-        get() {
-            var sum = 0
-            for (tour in this.tours) {
-                sum += tour.amountOfActivities
-            }
-
-            return sum
-        }
-
-    fun getTotalNumberOfActivitites(acttype: ActivityType?): Int {
-        var sum = 0
-        for (tour in this.tours) {
-            for (act in tour.activities) {
-                if (act.activitytypeisScheduled() && act.activityType == acttype) sum++
-            }
-        }
-        return sum
-    }
 
     val highestTourIndex: Int
         get() {
@@ -144,6 +57,120 @@ class HDay(parent: HWeekPattern, val weekday: DayOfWeek) {
 
     val lastTourOfDay: HTour
         get() = getTour(highestTourIndex)
+
+
+
+
+
+    val isHomeDay: Boolean
+        get() = (amountOfTours == 0)
+
+    val allActivitiesoftheDay: List<HActivity>
+        get() {
+            val allactivities: MutableList<HActivity> = ArrayList()
+            for (tmptour in this.tours) {
+                allactivities.addAll(tmptour.activities)
+            }
+            return allactivities
+        }
+
+    val mainTourType: ActivityType?
+        get() = getTour(0).getActivity(0).activityType
+
+
+    val totalAmountOfActivitites: Int
+        /**
+         * returns the amount of activities without home activities
+         *
+         * @return
+         */
+        get() {
+            var sum = 0
+            for (tour in this.tours) {
+                sum += tour.amountOfActivities
+            }
+
+            return sum
+        }
+    val amountOfTours: Int
+        get() {
+            return tours.size
+        }
+
+
+    val totalAmountOfActivityTime: Int
+        /**
+         * returns total activity duration on that day
+         *
+         * @return
+         */
+        get() {
+            var totalTime = 0
+            for (tour in this.tours) {
+                totalTime += tour.actDuration
+            }
+            return totalTime
+        }
+
+    val totalAmountOfTripTime: Int
+        /**
+         * returns total trip duration on that day
+         *
+         * @return
+         */
+        get() {
+            var totalTime = 0
+            for (tour in this.tours) {
+                totalTime += tour.tripDuration
+            }
+            return totalTime
+        }
+
+    val previousDay: HDay?
+        get() {
+            val previousDay = if (index == 0) {
+                null
+            } else {
+                pattern.getDay(index - 1)
+            }
+
+            return previousDay
+        }
+
+    val nextDay: HDay?
+        get() {
+            val nextDay = if (index == 6) {
+                null
+            } else {
+                pattern.getDay(index + 1)
+            }
+
+            return nextDay
+        }
+
+    fun hasActivity(activityType: ActivityType): Boolean {
+        return tours.any { t -> t.activities.any { it.activityType == activityType && it.isScheduled } }
+    }
+    fun addTour(tour: HTour) {
+        assert(tour.index != -99) { "index of the tour is not initialized" }
+        var tourindexexisitiert = false
+        for (tmptour in tours) {
+            if (tmptour.index == tour.index) tourindexexisitiert = true
+        }
+        assert(!tourindexexisitiert) { "a tour using this index already exists" }
+        tours.add(tour)
+    }
+    fun getTotalNumberOfActivitites(acttype: ActivityType): Int {
+        var sum = 0
+        for (tour in this.tours) {
+            for (act in tour.activities) {
+                if (act.activitytypeisScheduled() && act.activityType == acttype) sum++
+            }
+        }
+        return sum
+    }
+
+
 
     /**
      * @param index
@@ -220,71 +247,9 @@ class HDay(parent: HWeekPattern, val weekday: DayOfWeek) {
         return result
     }
 
-    val amountOfTours: Int
-        get() {
-            checkNotNull(tours) { "list of tour is not initialized" }
-            return tours.size
-        }
 
 
-    val totalAmountOfActivityTime: Int
-        /**
-         * returns total activity duration on that day
-         *
-         * @return
-         */
-        get() {
-            var totalTime = 0
-            for (tour in this.tours) {
-                totalTime += tour.actDuration
-            }
-            return totalTime
-        }
 
-    val totalAmountOfTripTime: Int
-        /**
-         * returns total trip duration on that day
-         *
-         * @return
-         */
-        get() {
-            var totalTime = 0
-            for (tour in this.tours) {
-                totalTime += tour.tripDuration
-            }
-            return totalTime
-        }
-
-
-    /**
-     * returns total tour time (activities + trips) from reference tour until last tour of the day
-     *
-     * @param referencePoint
-     * @return
-     */
-    fun getTotalAmountOfRemainingTourTime(referencePoint: HTour): Int {
-        var totalTime = 0
-
-        for (i in referencePoint.index..highestTourIndex) {
-            totalTime += getTour(i).tourDuration
-        }
-        return totalTime
-    }
-
-    /**
-     * returns total tour time (activities + trips) from reference tour until main tour of the day
-     *
-     * @param referencePoint
-     * @return
-     */
-    fun getTotalAmountOfTourTimeUntilMainTour(referencePoint: HTour): Int {
-        var totalTime = 0
-
-        for (i in referencePoint.index..-1) {
-            totalTime += getTour(i).tourDuration
-        }
-        return totalTime
-    }
 
 
     /**
@@ -319,28 +284,13 @@ class HDay(parent: HWeekPattern, val weekday: DayOfWeek) {
 
 
 
-    val previousDay: HDay?
-        get() {
-            val previousDay = if (index == 0) {
-                null
-            } else {
-                pattern.getDay(index - 1)
-            }
-
-            return previousDay
+    fun calculatedurationofmainactivitiesonday(): Int {
+        var totalActivityTime = 0
+        for (tour in this.tours) {
+            totalActivityTime += tour.getActivity(0).duration
         }
-
-    val nextDay: HDay?
-        get() {
-            val nextDay = if (index == 6) {
-                null
-            } else {
-                pattern.getDay(index + 1)
-            }
-
-            return nextDay
-        }
-
+        return totalActivityTime
+    }
     /**
      * @param name  specific attribute for map
      * @param value
@@ -356,11 +306,49 @@ class HDay(parent: HWeekPattern, val weekday: DayOfWeek) {
          */
         get() = attributes
 
-    fun calculatedurationofmainactivitiesonday(): Int {
-        var totalActivityTime = 0
-        for (tour in this.tours) {
-            totalActivityTime += tour.getActivity(0).duration
+
+    /**
+     * returns total tour time (activities + trips) from reference tour until last tour of the day
+     *
+     * @param referencePoint
+     * @return
+     */
+    fun getTotalAmountOfRemainingTourTime(referencePoint: HTour): Int {
+        var totalTime = 0
+
+        for (i in referencePoint.index..highestTourIndex) {
+            totalTime += getTour(i).tourDuration
         }
-        return totalActivityTime
+        return totalTime
+    }
+
+    /**
+     * returns total tour time (activities + trips) from reference tour until main tour of the day
+     *
+     * @param referencePoint
+     * @return
+     */
+    fun getTotalAmountOfTourTimeUntilMainTour(referencePoint: HTour): Int {
+        var totalTime = 0
+
+        for (i in referencePoint.index..-1) {
+            totalTime += getTour(i).tourDuration
+        }
+        return totalTime
+    }
+    override fun toString(): String {
+        return "Wochentag " + weekday +
+                " #Touren " + amountOfTours +
+                " Haupttour: " + mainTourType
+    }
+
+    fun printDayPattern(): String {
+        var result = "Day " + index + " // "
+
+        for (tmpact in allActivitiesoftheDay) {
+            result = result + " " + tmpact.index + " " + tmpact.activityType.typeasChar
+            if (tmpact.isActivityLastinTour) result = "$result //"
+        }
+        return result
     }
 }
